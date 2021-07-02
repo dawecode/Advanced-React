dd76a2524b82aa1e9840a17aacfcfe50b15482ac
Catherine Dawe <dawe.cm@gmail.com>
2021-07-02 09:53:14 -0400

    styling error

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,4 +1,11 @@
+import Nprogress from 'nprogress';
 import Page from '../components/Page';
+import '../components/styles/nprogress.css';
+import Router from 'next/router';
+
+Router.events.on('routeChangeStart', () => Nprogress.start());
+Router.events.on('routeChangeComplete', () => Nprogress.done());
+Router.events.on('routeChangeError', () => Nprogress.start());
 
 export default function MyApp({ Component, pageProps }) {
   return (
