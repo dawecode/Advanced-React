d1daf8f7d431c4f2a75234a918e6a1deb6cedba5
Catherine Dawe <dawe.cm@gmail.com>
2021-07-07 10:38:30 -0400

    correct css

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -25,7 +25,7 @@ const HeaderStyles = styled.header`
     display: grid;
     grid-template-columns: auto 1fr;
     justify-content: space-between;
-    align-items: center;
+    align-items: stretch;
   }
 
   .sub-bar {
@@ -40,13 +40,13 @@ export default function Header() {
     <HeaderStyles>
       <div className="bar">
         <Logo>
-          <Link href="/">sick fits</Link>
+          <Link href="/">Sick fits</Link>
         </Logo>
+      <Nav />
       </div>
       <div className="sub-bar">
-        <p> Search</p>
+        <p>Search</p>
       </div>
-      <Nav />
     </HeaderStyles>
   );
 }
