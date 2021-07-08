4054498d9267ac8cd7dc7fd2902a41971ca5c547
Catherine Dawe <dawe.cm@gmail.com>
2021-07-08 11:35:18 -0400

    add single product

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,27 +1,76 @@
 import React from "react";
+import gql from "graphql-tag";
+import { useMutation } from "@apollo/client"
 import useForm from "../lib/useForm";
 import Form from "./styles/Form";
+import DisplayError from "./ErrorMessage"
+import { ALL_PRODUCTS_QUERY } from "./Products"
+import Router from'next/router';
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    #Which variables are getting passed in and what types are they
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      price
+      description
+      name
+    }
+  }
+`;
 
 export default function CreateProduct() {
   const { inputs, handleChange, resetForm, clearForm } = useForm({
-    image: "",
+    image: '',
     name: "Nice shoes",
     price: 3500,
     description: "These are the best",
   });
 
+  const [createProduct, {loading,error,data}] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+     { 
+       variables: inputs,
+       refetchQueries: [{query:ALL_PRODUCTS_QUERY}],
+    }
+    );
+
+
   return (
-    <Form onSubmit={(e)=> {
-      e.preventDefault();
-      console.log(inputs)
-      }}>
-      <fieldset>
-      <label htmlFor="image">
+    <Form
+      onSubmit={ async (e) => {
+        e.preventDefault();
+       
+        //submit imput fields do back end
+         const res = await createProduct();
+         clearForm();
+        //go to that products page
+        Router.push({
+          pathname:`/product/${res.data.createProduct.id}`
+        })
+      }}
+    >
+      <DisplayError error={error}/>
+      <fieldset disabled={loading}  aria-busy={loading}>
+        <label htmlFor="image">
           Image
           <input
             required
             type="file"
-            id="name"
+            id="image"
             name="image"
             onChange={handleChange}
           />
