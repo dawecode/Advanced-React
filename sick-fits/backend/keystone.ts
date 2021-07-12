1289b1eabb8006c0bc436c75d7bda7997fc38ee6
Catherine Dawe <dawe.cm@gmail.com>
2021-07-12 18:10:37 -0400

    add email and cart info

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -4,11 +4,13 @@ import { createAuth } from "@keystone-next/auth";
 import { User } from "./schemas/User";
 import { Product } from "./schemas/Product";
 import { ProductImage } from "./schemas/ProductImage";
+import { CartItem } from "./schemas/CartItem";
 import { insertSeedData } from "./seed-data";
 import {
   withItemData,
   statelessSessions,
 } from "@keystone-next/keystone/session";
+import { sendPasswordResetEmail } from "./lib/mail";
 
 const databaseURL =
   process.env.DATABASE_URL || "mongodb://localhost/keystone-sick-fits-tutorial";
@@ -24,6 +26,12 @@ const { withAuth } = createAuth({
   secretField: "password",
   initFirstItem: {
     fields: ["name", "email", "password"],
+  },
+  passwordResetLink: {
+    async sendToken(args) {
+      // send email
+      await sendPasswordResetEmail(args.token, args.identity);
+    },
   },
 });
 export default withAuth(
@@ -50,6 +58,7 @@ export default withAuth(
       User,
       Product,
       ProductImage,
+      CartItem,
     }),
     ui: {
       //show the ui only for people who pass this test
